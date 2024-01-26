1e8ea7585ddd8f4ef4993e9dca37d67c96d94ce7
Chandan Kumar <chandan.raj4@gmail.com>
2024-01-26 22:16:21 +0530

    SQS-lambda test initial commit

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,10 @@
-async function hello(event, context) {
+async function hello(event) {
+  console.log(JSON.stringify(event, null, 2));
+  // if we return the success message here from lambda,
+  // messages in queue gets deleted.
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from Chandan Kumar' }),
+    body: JSON.stringify({ message: 'Hello World' }),
   };
 }
 
